9f9d555b8a17f4cdb86085bd6456d237000bfa78
DeZoST <zosted62400@gmail.com>
2023-09-16 23:06:10 +0200

    Ajout de l'email dans le success message

--- script.js
+++ script.js
@@ -5,44 +5,36 @@ const successMessage = document.getElementById("success__message");
 const newslatterCard = document.getElementById("newslatter__card");
 const success__button = document.getElementById("success__button");
 
-
+const success__email = document.getElementById("success__email");
 
 form.addEventListener("submit", (e) => {
-    if(!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/) || email.value == "" || email.value == null){
-        emailError.setAttribute(
-            "style", "display: inline;"
-        )
-        email.setAttribute(
-            "style", "border: 1px solid var(--clr-tomato); color: var(--clr-tomato); background-color: var(--clr-tomato-light);"
-        )
-        e.preventDefault();
-    } else{
+  if (
+    !email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/) ||
+    email.value == "" ||
+    email.value == null
+  ) {
+    emailError.setAttribute("style", "display: inline;");
+    email.setAttribute(
+      "style",
+      "border: 1px solid var(--clr-tomato); color: var(--clr-tomato); background-color: var(--clr-tomato-light);"
+    );
+    e.preventDefault();
+  } else {
 
-        emailError.setAttribute(
-            "style", "display: none;"
-        )
+    success__email.innerText = email.value;
 
-        email.setAttribute(
-            "style", "border: 1px solid var(--clr-grey);"
-        )
+    emailError.setAttribute("style", "display: none;");
 
-        newslatterCard.setAttribute(
-            "style", "display: none;"
-        )
+    email.setAttribute("style", "border: 1px solid var(--clr-grey);");
 
-        successMessage.setAttribute(
-            "style", "display: flex;"
-        )
-        
-    }
+    newslatterCard.setAttribute("style", "display: none;");
+
+    successMessage.setAttribute("style", "display: flex;");
+  }
 });
 
 success__button.addEventListener("click", () => {
-    successMessage.setAttribute(
-        "style", "display: none;"
-    )
+  successMessage.setAttribute("style", "display: none;");
 
-    newslatterCard.setAttribute(
-        "style", "display: flex;"
-    )
-})
+  newslatterCard.setAttribute("style", "display: flex;");
+});
